1d487df7addaf0615c6d496affc6b548fde19cff
vadim2071 <vadim207138@gmail.com>
2021-09-16 00:11:23 +0300

    Lesson03 begin SQL

---
+++ MetricsManager/MetricsAgent/Model/CpuMetric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsAgent.Model
+{
+    public class CpuMetric
+    {
+        public int Id { get; set; }
+
+        public int Value { get; set; }
+
+        public TimeSpan Time { get; set; }
+    }
+}
