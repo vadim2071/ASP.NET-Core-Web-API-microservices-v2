9b1bb39703203063fb6ab391271c73ea2619cd6a
IVG <vadim207138@gmail.com>
2021-09-18 00:58:06 +0300

    update

---
+++ MetricsManager/MetricsAgent/Request/DotNetMetricCreateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsAgent.Request
+{
+    public class DotNetMetricCreateRequest
+    {
+        public TimeSpan Time { get; set; }
+        public int Value { get; set; }
+    }
+}
