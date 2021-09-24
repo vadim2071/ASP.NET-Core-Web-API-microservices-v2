02ff24109c36c39daae8d1a6392fc88d5dd642d8
vadim2071 <vadim207138@gmail.com>
2021-09-24 22:05:47 +0300

    update

--- MetricsManager/MetricsAgent/Controllers/CpuMetricsController.cs
+++ MetricsManager/MetricsAgent/Controllers/CpuMetricsController.cs
@@ -26,6 +26,75 @@ namespace MetricsAgent.Controllers
             this.repository = repository;
         }
 
+        [HttpGet("sql-read-write-test")]
+        public IActionResult TryToInsertAndRead()
+        {
+            repository.Create()
+
+            // создаем соединение с базой данных
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                // открываем соединение
+                connection.Open();
+
+                // создаем объект через который будут выполняться команды к базе данных
+                using (var command = new SQLiteCommand(connection))
+                {
+                    // задаем новый текст команды для выполнения
+                    // удаляем таблицу с метриками если она существует в базе данных
+                    command.CommandText = "DROP TABLE IF EXISTS cpumetrics";
+                    // отправляем запрос в базу данных
+                    command.ExecuteNonQuery();
+
+                    // создаем таблицу с метриками
+                    command.CommandText = @"CREATE TABLE cpumetrics(id INTEGER PRIMARY KEY,
+                    value INT, time INT)";
+                    command.ExecuteNonQuery();
+
+                    // создаем запрос на вставку данных
+                    command.CommandText = "INSERT INTO cpumetrics(value, time) VALUES(10,1)";
+                    command.ExecuteNonQuery();
+                    command.CommandText = "INSERT INTO cpumetrics(value, time) VALUES(50,2)";
+                    command.ExecuteNonQuery();
+                    command.CommandText = "INSERT INTO cpumetrics(value, time) VALUES(75,4)";
+                    command.ExecuteNonQuery();
+                    command.CommandText = "INSERT INTO cpumetrics(value, time) VALUES(90,5)";
+                    command.ExecuteNonQuery();
+
+                    // создаем строку для выборки данных из базы
+                    // LIMIT 3 обозначает, что мы достанем только 3 записи
+                    string readQuery = "SELECT * FROM cpumetrics LIMIT 3";
+
+                    // создаем массив, в который запишем объекты с данными из базы данных
+                    var returnArray = new CpuMetric[3];
+                    // изменяем текст команды на наш запрос чтения
+                    command.CommandText = readQuery;
+
+                    // создаем читалку из базы данных
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        // счетчик для того, чтобы записать объект в правильное место в массиве
+                        var counter = 0;
+                        // цикл будет выполняться до тех пор, пока есть что читать из базы данных
+                        while (reader.Read())
+                        {
+                            // создаем объект и записываем его в массив
+                            returnArray[counter] = new CpuMetric
+                            {
+                                Id = reader.GetInt32(0), // читаем данные полученные из базы данных
+                                Value = reader.GetInt32(1), // преобразуя к целочисленному типу
+                                Time = reader.GetInt64(2)
+                            };
+                            // увеличиваем значение счетчика
+                            counter++;
+                        }
+                    }
+                    // оборачиваем массив с данными в объект ответа и возвращаем пользователю 
+                    return Ok(returnArray);
+                }
+            }
+        }
+
         [HttpGet("from/{fromTime}/to/{toTime}")]
         public IActionResult GetMetrics([FromRoute] TimeSpan fromTime,[FromRoute] TimeSpan toTime)
         {
