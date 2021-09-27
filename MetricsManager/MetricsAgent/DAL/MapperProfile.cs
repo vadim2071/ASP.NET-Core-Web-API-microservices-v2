3315fdc3d62a2487f67cd6d1271b59ee617a2087
vadim2071 <vadim207138@gmail.com>
2021-09-27 23:53:28 +0300

    Add Mapper

---
+++ MetricsManager/MetricsAgent/DAL/MapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MetricsAgent.DAL.Models;
+using MetricsAgent.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsAgent.DAL
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            // добавлять сопоставления в таком стиле нужно для всех объектов 
+            CreateMap<CpuMetric, CpuMetricDTO>(); //!!!!!!!!!!!! Сделать абстрактный класс и BaseMetricDto!!!!!!!!!!!!
+        }
+    }
+}
